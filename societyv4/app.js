482936c5a1ac70cb17d1680f2ca959927a2969c1
neelrathod <neel.rathod.sa@gmail.com>
2017-10-27 19:25:43 +0530

    admin completed

---
+++ societyv4/app.js
@@ -0,0 +1,48 @@
+var express = require("express"),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    cors = require("cors"),
+    path = require("path")
+
+var app = express();
+
+//Port No.
+const port = 4000 || process.nev.PORT ;
+
+//MongoDb Connection
+mongoose.connect("mongodb://localhost/my_society");
+
+//Routes
+const routes = require('./routes/route');
+
+
+
+//On Connection
+mongoose.connection.on("connected", function (req, res) {
+    console.log("Connected to Database @27017")
+});
+
+mongoose.connection.on("error", function (err) {
+    if (err) {
+        console.log("Error in database connection " + err)
+    }
+
+});
+
+//Adding Middle-ware - cors => It's used for client side port 
+app.use(cors());
+
+//body-parser
+app.use(bodyParser.json());
+
+//Routes 
+app.use("/api", routes);
+
+app.get("/", (req, res)=>{
+    res.send("Connected")
+});
+
+
+app.listen(port, process.env.IP || function(){
+    console.log("Server is started at port : " + port)
+});
---
+++ societyv4/client/src/app/app-routing-module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule} from "@angular/router";
+import { LoginComponent } from "./user/login/login.component";
+import { SignupComponent } from "./user/signup/signup.component";
+import { HeaderComponent } from "./header/header.component";
+import { AdminComponent } from "./admin/admin.component";
+import { VehiclesComponent } from "./vehicles/vehicles.component";
+import { ListComponent } from "./vehicles/list/list.component";
+import { AddComponent } from "./vehicles/add/add.component";
+import { MyvehicleComponent } from "./vehicles/myvehicle/myvehicle.component";
+
+
+const appRoutes : Routes =[
+    { path: '', component : LoginComponent },
+    { path: 'signup', component: SignupComponent},  
+    {path: 'header', component : HeaderComponent, children: [
+        {path : 'vehicles', component : VehiclesComponent},
+        {path: 'list', component : ListComponent},
+        {path: 'add', component: AddComponent},
+        {path : 'myvehicle/:id', component : MyvehicleComponent}
+    ]},
+    {path: 'admin', component : AdminComponent}
+    
+        
+    
+
+    
+  ];
+  
+@NgModule({
+    imports:[
+        // RouterModule.forRoot(appRoutes, {useHash: true})
+
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports:[RouterModule]
+})
+
+export class AppRoutingModule {
+    
+}
